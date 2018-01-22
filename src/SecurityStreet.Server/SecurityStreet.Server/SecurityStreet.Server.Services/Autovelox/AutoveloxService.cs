62381706a955f9847eb7ea94edc458dc395e7c9d
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2018-01-22 18:51:42 +0100

    Added GetAutoveloxByDistance request

--- src/SecurityStreet.Server/SecurityStreet.Server/SecurityStreet.Server.Services/Autovelox/AutoveloxService.cs
+++ src/SecurityStreet.Server/SecurityStreet.Server/SecurityStreet.Server.Services/Autovelox/AutoveloxService.cs
@@ -1,9 +1,55 @@
 ﻿using SecurityStreet.Server.Models.Dto;
 using ServiceStack;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
 
 namespace SecurityStreet.Server.Services.Autovelox
 {
+    /// <summary>
+    /// The autovelox serivce
+    /// </summary>
+    /// <seealso cref="SecurityStreet.Server.Services.BaseService{SecurityStreet.Server.Models.Entities.Autovelox, SecurityStreet.Server.Models.Dto.AutoveloxDto, SecurityStreet.Server.Services.Autovelox.ReadAutoveloxRequest}" />
     public class AutoveloxService : BaseService<Server.Models.Entities.Autovelox, AutoveloxDto, ReadAutoveloxRequest>
     {
+        /// <summary>
+        /// Gets all the autovelox inside a centered circle the given coordinates and distance
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        public List<AutoveloxDto> Any(ReadAutoveloxByDistanceRequest request)
+        {
+
+            if (request == null)
+                throw new ArgumentException("Request cannot be null");
+
+            if (request.Latitude == 0 || request.Longitude == 0)
+                throw new ArgumentException("Coordinates cannot be null");
+
+            if (request.Distance <= 0)
+                request.Distance = 100;
+
+            var firstCoord = new GeoCoordinate(request.Latitude, request.Longitude);
+
+            using (var db = dbConnectionFactory.Open())
+            {
+                try
+                {
+                    var q = db.SelectLazy<Server.Models.Entities.Autovelox>()
+                        .Where(a => new GeoCoordinate(a.Latitude, a.Longitude).GetDistanceTo(firstCoord) <= (request.Distance * 1000));
+
+                    List<AutoveloxDto> results = new List<AutoveloxDto>();
+                    q.Each(a => results.Add(a.ConvertTo<AutoveloxDto>()));
+
+                    return results;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
     }
 }
---
+++ src/SecurityStreet.Server/SecurityStreet.Server/SecurityStreet.Server.Services/Autovelox/Models/ReadAutoveloxByDistanceRequest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ServiceStack;
+using SecurityStreet.Server.Models.Dto;
+using SecurityStreet.Server.Services.Models;
+
+namespace SecurityStreet.Server.Services.Autovelox
+{
+    /// <summary>
+    /// Gets all the autovelox inside a centered circle the given coordinates and distance
+    /// </summary>
+    /// <seealso cref="ServiceStack.IReturn{System.Collections.Generic.List{SecurityStreet.Server.Models.Dto.AutoveloxDto}}" />
+    [Route("/autovelox/readByDistance/{Latitude}/{Longitude}/{Distance}", "GET")]
+    [Route("/autovelox/readByDistance/{Latitude}/{Longitude}", "GET")]
+    public class ReadAutoveloxByDistanceRequest : IReturn<List<AutoveloxDto>>
+    {
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        /// <value>
+        /// The latitude.
+        /// </value>
+        public float Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        /// <value>
+        /// The longitude.
+        /// </value>
+        public float Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the distance in KM.
+        /// </summary>
+        /// <value>
+        /// The distance.
+        /// </value>
+        public int Distance { get; set; }
+
+    }
+}
